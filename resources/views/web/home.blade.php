725fb81db11d5b2b8212afdb5e07b7d1eabe96f2
ariany-f <ariany_f@hotmail.com>
2026-02-06 01:49:51 -0300

    Update color values for button and text styles in CSS and SCSS files
    - Changed color values for .btn-purple and .text-purple from #8635fd to #ba0101 to align with the updated color scheme.
    - Ensured consistency across multiple CSS and SCSS files, enhancing the visual appearance of buttons and text elements throughout the application.

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -51,7 +51,7 @@
                                 class="w-full sm:min-h-[430px] min-h-[365px] sm:max-w-full max-w-[300px] shadow-[0px_2.48px_28.06px_0px_#00000017] rounded-[20px] px-4 sm:px-5 py-6 sm:py-7 bg-white mx-auto">
                                 <div class="pb-5">
                                     <div
-                                        class="lg:w-[92px] lg:h-[92px] sm:w-[80px] sm:h-[80px] w-[60px] h-[60px] rounded-full bg-primary flex justify-center items-center mx-auto mb-4 sm:shadow-[6px_3px_0_0_#8635fd70] shadow-[5px_2px_0_0_#8635fd70]">
+                                        class="lg:w-[92px] lg:h-[92px] sm:w-[80px] sm:h-[80px] w-[60px] h-[60px] rounded-full bg-primary flex justify-center items-center mx-auto mb-4 sm:shadow-[6px_3px_0_0_#ba010170] shadow-[5px_2px_0_0_#ba010170]">
                                         <img src="{{ $service->icon }}" alt="feature-img"
                                             class="lg:w-12 lg:h-12 sm:w-10 sm:h-10 w-8 h-8 object-cover" />
                                     </div>
@@ -474,7 +474,7 @@
                     <div
                         class="relative after:absolute after:content-[' '] after:max-h-5 sm:after:max-w-[118px] after:w-full after:h-full after:max-w-20 after:bg-no-repeat after:bg-contain after:rotate-90 sm:after:rotate-0 {{ $loop->last ? '' : "after:bg-[url('../assets/images/setp-after.png')]"}} after:-bottom-[56px] sm:after:bottom-auto sm:after:top-5 after:right-0 after:left-0 sm:after:left-auto after:mx-auto sm:after:mx-0 sm:after:!-right-20">
                         <div
-                            class="sm:shadow-[6px_3px_0_0_#8635fd70] shadow-[5px_2px_0_0_#8635fd70] w-full h-full bg-primary rounded-full max-w-[60px] sm:max-w-[84px] max-h-[60px] sm:max-h-[84px] min-h-[60px] sm:min-h-[84px] mx-auto flex items-center justify-center mb-4 sm:mb-6 lg:mb-[32px] relative after:absolute after:content-[' '] after:-right-1.5 after:top-0.5 after:h-full after:w-full after:rounded-full after:-z-[1] after:bg-primary-light">
+                            class="sm:shadow-[6px_3px_0_0_#ba010170] shadow-[5px_2px_0_0_#ba010170] w-full h-full bg-primary rounded-full max-w-[60px] sm:max-w-[84px] max-h-[60px] sm:max-h-[84px] min-h-[60px] sm:min-h-[84px] mx-auto flex items-center justify-center mb-4 sm:mb-6 lg:mb-[32px] relative after:absolute after:content-[' '] after:-right-1.5 after:top-0.5 after:h-full after:w-full after:rounded-full after:-z-[1] after:bg-primary-light">
                             <div class="max-w-7 sm:max-w-10 max-h-7 sm:max-h-10 w-full h-full">
                                 <img src="{{ $step->image }}" alt="{{ $step->title }}"
                                     class="h-full w-full object-cover" />
@@ -616,7 +616,7 @@
                                     viewBox="0 0 18 24" fill="none">
                                     <path
                                         d="M9 0C6.61392 0.00294898 4.32637 0.997261 2.63915 2.76482C0.951939 4.53238 0.0028245 6.92886 9.57508e-06 9.42857C-0.00284824 11.4713 0.634084 13.4587 1.8131 15.0857C1.8131 15.0857 2.05855 15.4243 2.09864 15.4731L9 24L15.9046 15.4689C15.9406 15.4234 16.1869 15.0857 16.1869 15.0857L16.1877 15.0831C17.3661 13.4568 18.0028 11.4704 18 9.42857C17.9972 6.92886 17.0481 4.53238 15.3608 2.76482C13.6736 0.997261 11.3861 0.00294898 9 0ZM9 12.8571C8.35272 12.8571 7.71997 12.6561 7.18177 12.2793C6.64358 11.9026 6.2241 11.3671 5.9764 10.7406C5.72869 10.1141 5.66388 9.42477 5.79016 8.75969C5.91644 8.09461 6.22814 7.4837 6.68583 7.00421C7.14353 6.52471 7.72668 6.19817 8.36152 6.06588C8.99637 5.93359 9.6544 6.00148 10.2524 6.26098C10.8504 6.52048 11.3616 6.95993 11.7212 7.52376C12.0808 8.08759 12.2727 8.75046 12.2727 9.42857C12.2716 10.3375 11.9265 11.2089 11.313 11.8517C10.6994 12.4944 9.86765 12.856 9 12.8571Z"
-                                        fill="#8635fd" />
+                                        fill="#ba0101" />
                                 </svg>
 
                             </div>
@@ -628,7 +628,7 @@
                                     viewBox="0 0 18 18" fill="none">
                                     <path
                                         d="M3 0C2.20435 0 1.44129 0.31607 0.87868 0.87868C0.31607 1.44129 0 2.20435 0 3L0 5.4C0 12.3588 5.6412 18 12.6 18H15C15.7957 18 16.5587 17.6839 17.1213 17.1213C17.6839 16.5587 18 15.7957 18 15V13.3416C18 13.0071 17.9068 12.6793 17.7309 12.3949C17.5549 12.1104 17.3032 11.8806 17.004 11.7312L14.106 10.2816C13.8753 10.1663 13.6224 10.1018 13.3647 10.0927C13.1069 10.0836 12.8501 10.13 12.6118 10.2288C12.3735 10.3276 12.1593 10.4764 11.9835 10.6652C11.8078 10.8541 11.6747 11.0784 11.5932 11.3232L11.2356 12.3948C11.1698 12.5915 11.0345 12.7574 10.8551 12.8614C10.6758 12.9655 10.4646 13.0005 10.2612 12.96C8.9729 12.7023 7.78967 12.0691 6.86056 11.1402C5.93144 10.2113 5.29799 9.02824 5.04 7.74C4.99949 7.53662 5.03454 7.32545 5.13859 7.14607C5.24264 6.96669 5.40855 6.83141 5.6052 6.7656L6.936 6.3216C7.36607 6.17799 7.7261 5.87735 7.94412 5.4798C8.16214 5.08224 8.2221 4.61705 8.112 4.1772L7.4088 1.3644C7.31168 0.975022 7.08723 0.629264 6.77111 0.382058C6.45498 0.134853 6.06531 0.000378443 5.664 0L3 0Z"
-                                        fill="#8635fd" />
+                                        fill="#ba0101" />
                                 </svg>
                             </div>
                             <a href="tel:{{ getSadminSettingValue('phone') }}" class="text-base
@@ -641,7 +641,7 @@
                                     viewBox="0 0 20 16" fill="none">
                                     <path
                                         d="M2 16C1.45 16 0.979333 15.8043 0.588 15.413C0.196 15.021 0 14.55 0 14V2C0 1.45 0.196 0.979333 0.588 0.588C0.979333 0.196 1.45 0 2 0H18C18.55 0 19.021 0.196 19.413 0.588C19.8043 0.979333 20 1.45 20 2V14C20 14.55 19.8043 15.021 19.413 15.413C19.021 15.8043 18.55 16 18 16H2ZM10 8.825C10.0833 8.825 10.1707 8.81233 10.262 8.787C10.354 8.76233 10.4417 8.725 10.525 8.675L17.6 4.25C17.7333 4.16667 17.8333 4.06267 17.9 3.938C17.9667 3.81267 18 3.675 18 3.525C18 3.19167 17.8583 2.94167 17.575 2.775C17.2917 2.60833 17 2.61667 16.7 2.8L10 7L3.3 2.8C3 2.61667 2.70833 2.61233 2.425 2.787C2.14167 2.96233 2 3.20833 2 3.525C2 3.69167 2.03333 3.83733 2.1 3.962C2.16667 4.08733 2.26667 4.18333 2.4 4.25L9.475 8.675C9.55833 8.725 9.646 8.76233 9.738 8.787C9.82933 8.81233 9.91667 8.825 10 8.825Z"
-                                        fill="#8635fd" />
+                                        fill="#ba0101" />
                                 </svg>
                             </div>
                             <a href="mailto:{{ getSadminSettingValue('email') }}" class="text-base  font-normal">
